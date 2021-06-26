783c9d26b3f2f8c69744487f08a299398938dbef
Wallace Antunes <wallace.antunes@ubiqui.com.br>
2021-06-26 17:44:23 -0300

    adding save and edit on modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="modalCreate" class="modal">
-    <div class="modal-header">
-      <div class="row">
-        <div class="col s12">
-          <h5>{{ title }} Contato</h5>
-        </div>
-      </div>
-    </div>
-    <div class="modal-content">
-      <form class="col s12" id="formContact">
+  <aside ref="modalCreate" class="modal">
+    <form @submit.prevent="submitForm">
+      <header class="modal-header">
         <div class="row">
-         <div class="input-field col s6">
+          <div class="col s12">
+            <h5>{{ title }} Contato</h5>
+          </div>
+        </div>
+      </header>
+      <div class="modal-content">
+        <div class="row">
+          <div class="input-field col s6">
             <input
               id="contact_name"
               v-model="name"
@@ -18,7 +18,7 @@
               type="text"
               required
               class="validate"
-            >
+            />
             <label for="contact_name">Nome</label>
           </div>
           <div class="input-field col s6">
@@ -26,19 +26,31 @@
               id="contact_phone"
               v-model="phone"
               type="text"
+              v-mask="'(##)#####-####'"
               required
               class="validate"
-            >
+            />
             <label for="contact_phone">Telefone</label>
           </div>
         </div>
-      </form>
-    </div>
-    <div class="modal-footer">
-      <a class="modal-close waves-effect waves-green btn-flat" @click="closeModal">Cancelar</a>
-      <a class="waves-effect waves-light btn green darken-1" @click="submitForm">{{title}}</a>
-    </div>
-  </div>
+      </div>
+      <footer class="modal-footer">
+        <button
+          class="modal-close waves-effect waves-green btn-flat"
+          type="button"
+          @click="closeModal"
+        >
+          Cancelar
+        </button>
+        <button
+          class="waves-effect waves-light btn green darken-1"
+          type="submit"
+        >
+          {{ title }}
+        </button>
+      </footer>
+    </form>
+  </aside>
 </template>
 
 <script>
@@ -53,6 +65,7 @@ export default {
     return {
       title: 'Cadastrar',
       isEditing: false,
+      id: null,
       name: null,
       phone: null,
     };
@@ -60,7 +73,7 @@ export default {
   watch: {
     openModal(value) {
       if (value) {
-        const elems = document.getElementById('modalCreate');
+        const elems = this.$refs.modalCreate;
         const instances = M.Modal.init(elems, {
           dismissible: false,
         });
@@ -70,6 +83,7 @@ export default {
     edit(value) {
       if (value) {
         this.isEditing = true;
+        this.id = value.id;
         this.name = value.name;
         this.phone = value.phone;
         this.title = 'Editar';
@@ -78,19 +92,38 @@ export default {
   },
   methods: {
     saveContact() {
-      console.log('save');
+      const data = {
+        name: this.name,
+        phone: this.phone,
+      };
+      this.$store.dispatch('addContact', data);
+      this.closeModal();
     },
     editContact() {
-      console.log('edit');
+      const data = {
+        id: this.id,
+        name: this.name,
+        phone: this.phone,
+      };
+      this.$store.dispatch('updateContact', data);
+      this.closeModal();
     },
     submitForm() {
       if (!this.isEditing) {
-        document.getElementById('formContact').submit(this.saveContact());
+        this.saveContact();
       } else {
-        document.getElementById('formContact').submit(this.editContact());
+        this.editContact();
       }
     },
     closeModal() {
+      this.id = null;
+      this.name = null;
+      this.phone = null;
+      const elems = this.$refs.modalCreate;
+      const instances = M.Modal.init(elems, {
+        dismissible: false,
+      });
+      instances.close();
       this.$emit('closeModal');
     },
   },
